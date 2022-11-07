f628a9d102ac0143f15aaeb09e558283a29684c9
Viviane Mayra <vivi.mayra01@gmail.com>
2022-11-07 20:09:26 -0300

    Refatorando efento de click no botão

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+//Função responsável pela abertura do menu
 
+let myButton=document.querySelector('button');
+let sidebar=document.querySelector('.sidebar');
+
+console.log(sidebar)
+myButton.addEventListener('click', () => {
+if(sidebar.getAttribute('class') ==='closed'){
+    console.log("oii")
+    sidebar.setAttribute('class', 'open');
+}else{
+    console.log("teteuuu")
+    sidebar.setAttribute('class', 'closed');
+
+}
+
+
+
+})
 
